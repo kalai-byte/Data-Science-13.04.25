66c52383d26c48866ee26a07078cd18a87e48fdf
kalai-byte <kalaivananv12@gmail.com>
2025-07-12 21:57:01 +0530

    project

---
+++ projectpolice.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import streamlit as st
+import psycopg2 as ps
+sheet_id='16qEUKckLFy7j1qoSd1bSQURn9QXDb6ffSBAWzRkPsSg'
+sheet_name='Sheet1'
+url  = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+traffic_df = pd.read_csv(url, low_memory=False)
+db_url= 'postgresql://kalai_online:zo0qcDMN4PGM84TQMWbAE2aQc6V2S8AA@dpg-d1l43iqdbo4c73aki3g0-a.singapore-postgres.render.com/traffic_stops'
+from sqlalchemy import create_engine,inspect
+engine=create_engine(db_url)
+traffic_df.to_sql('traffic_stops',engine,index=False,if_exists='replace')
+Qurey="Select * from traffic_stops WHERE driver_age>50"
+new_traffic_df=pd.read_sql(Qurey,engine)
+print(new_traffic_df)
+st.set_page_config(page_title="Dashboard")
+
+#Data Cleaning
+
+traffic_df.dropna(axis=1, how='all', inplace=True)
+
+traffic_df.fillna({
+    'driver_age': traffic_df['driver_age'].median(),
+    'search_type': 'None',
+    'stop_duration':'unknown',
+    'violation':'unknown',
+    'stop_outcome':'unknown',
+}, inplace=True)
+
+traffic_df['timestamp'] = pd.to_datetime(traffic_df['stop_date'] +' '+ traffic_df['stop_time'])
+traffic_df['stop_date']= pd.to_datetime(traffic_df['stop_date'], errors='coerce').dt.strftime('%Y-%m-%d')
+traffic_df['stop_date']= pd.to_datetime(traffic_df['stop_date'], format='%H:%M:%S', errors='coerce').dt.strftime('H:%M:%S')
+
+query_list = st.selectbox("Select a Query to Run", [
+    "Total Number of Police Stops",
+    "Count of Stops by Violation Type",
+    "Number of Arrests vs Warnings",
+    "Average Age of Drivers Stopped",
+    "Top 5 Most Frequent Search Types",
+    "Count of Stops by Gender",
+    "Most Common Violation for Arrests"
+])
+
+query_ans = {
+    "Total Number of Police Stops": "SELECT COUNT(*) AS total_stops FROM police_stops",
+    "Count of Stops by Violation Type": "SELECT violation, COUNT(*) AS count FROM police_stops GROUP BY violation ORDER BY count DESC",
+    "Number of Arrests vs Warnings": "SELECT stop_outcome, COUNT(*) AS count FROM police_stops GROUP BY stop_outcome",
+    "Average Age of Drivers Stopped": "SELECT AVG(driver_age) AS average_age FROM police_stops",
+    "Top 5 Most Frequent Search Types": "SELECT search_type, COUNT(*) FROM police_stops WHERE search_type != '' GROUP BY search_type ORDER BY COUNT(*) DESC LIMIT 5",
+    "Count of Stops by Gender": "SELECT driver_gender, COUNT(*) AS count FROM police_stops GROUP BY driver_gender",
+    "Most Common Violation for Arrests": "SELECT violation, COUNT(*) AS count FROM police_stops WHERE stop_outcome LIKE '%arrest%' GROUP BY violation ORDER BY count DESC LIMIT 10"
+}
